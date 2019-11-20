53db9fb53fcc08b55a1e41ad3673b4db8a04766e
Bullas Attekita <bullas.atk@gmail.com>
2019-11-20 08:17:47 -0300

    Aula 01
    Commit inicial arquivos da Aula 01

---
+++ Game/Aula01/Assets/Game/Scripts/BackgroundScaler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScaler : MonoBehaviour
+{
+
+    private Vector3 startScale;
+
+
+    void Start()
+    {
+        startScale = transform.localScale;
+
+        float height = Camera.main.orthographicSize * 2;
+        float width = height * Screen.width / Screen.height;
+
+        transform.localScale = new Vector3(width, height, startScale.z);
+    }
+
+}
---
+++ Game/Aula01/Assets/Game/Scripts/BackgroundTextureAnimator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundTextureAnimator : MonoBehaviour
+{
+
+    private Material mat;
+    private Vector2 offset;
+
+
+    void Start()
+    {
+        mat = gameObject.GetComponent<Renderer>().material;
+        offset = mat.GetTextureOffset("_MainTex");
+
+    }
+
+    void Update()
+    {
+        offset.x = offset.x + (0.2f * Time.deltaTime);
+        mat.SetTextureOffset("_MainTex", offset);
+    }
+}
