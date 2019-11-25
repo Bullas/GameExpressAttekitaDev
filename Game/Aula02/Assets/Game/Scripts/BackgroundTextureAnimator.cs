0ce45cf865e524875782fac52540a336955cda20
Bullas Attekita <bullas.atk@gmail.com>
2019-11-25 20:25:39 -0300

    Aula 02
    - Implememtando o chão do cenário
    - Trabalhando camadas de background
    - Criando obstaculos

---
+++ Game/Aula02/Assets/Game/Scripts/BackgroundTextureAnimator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundTextureAnimator : MonoBehaviour
+{
+    private Material mat;
+    private Vector2 offset;
+
+    public float speed = 0.2f;
+
+    void Start()
+    {
+        mat = gameObject.GetComponent<Renderer>().material;
+        offset = mat.GetTextureOffset("_MainTex");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset.x = offset.x + (speed * Time.deltaTime);
+        mat.SetTextureOffset("_MainTex", offset);
+    }
+}
