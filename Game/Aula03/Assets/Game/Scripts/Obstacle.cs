9c1999bd5acd6b315f3a29016770d2265d786b8c
Bullas Attekita <bullas.atk@gmail.com>
2019-12-05 10:37:16 -0300

    Aula 03
    Adicionando interatividade ao game e tratando os inputs do player. Adicionando movimentação aos obstaculos.

---
+++ Game/Aula03/Assets/Game/Scripts/Obstacle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    private Rigidbody2D objRB;
+
+    void Start()
+    {
+        objRB = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        objRB.velocity = new Vector2(-5f, 0f);
+    }
+}
