bb34d611998b84b1caea77540744fb38b44b3ad2
Bullas Attekita <bullas.atk@gmail.com>
2019-12-12 14:51:40 -0300

    Aula 04
    Criando um script gerador de obstaculos e otimizando para o game

---
+++ Game/Aula04/Assets/Game/Scripts/BackgroundScaler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScaler : MonoBehaviour
+{
+    private Vector3 startScale;
+
+    public enum ScaleType
+    {
+        x,
+        y,
+        all
+    }
+
+    public ScaleType scaleType = ScaleType.all;
+
+
+    void Start()
+    {
+        startScale = transform.localScale;
+
+        FitToScreen();
+        
+    }
+
+    void FitToScreen()
+    {
+        float height = Camera.main.orthographicSize * 2;
+        float width = height * Screen.width / Screen.height;
+
+        float newWidth = (scaleType == ScaleType.all || scaleType == ScaleType.x) ? width : startScale.x;
+        float newHeight = (scaleType == ScaleType.all || scaleType == ScaleType.y) ? height : startScale.y;
+
+        transform.localScale = new Vector3(newWidth, newHeight, startScale.z);
+    }
+
+
+}
---
+++ Game/Aula04/Assets/Game/Scripts/Player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D plyRB;
+    private Animator animator;
+    private bool canJump;
+
+    public float jumpFactor = 5f;
+    public float forwardFactor = 0.2f;
+    private float forwardForce = 0f;
+
+    void Start()
+    {
+        plyRB = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
+        canJump = true;
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        if (canJump)
+        {
+            canJump = false;
+
+            if(transform.position.x < 0)
+            {
+                forwardForce = forwardFactor;
+            }
+            else
+            {
+                forwardForce = 0f;
+            }
+
+            plyRB.velocity = new Vector2(forwardForce, jumpFactor);
+            animator.Play("player_jumping");
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        canJump = true;
+        animator.Play("player_running");
+    }
+}
