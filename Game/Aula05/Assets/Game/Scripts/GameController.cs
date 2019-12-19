c222d775e702320972248dec2e11c35c9e1c9a12
Bullas Attekita <bullas.atk@gmail.com>
2019-12-19 15:29:51 -0300

    Aula 05
    Tratando os estados do nosso jogo e criando o Game Over.

---
+++ Game/Aula05/Assets/Game/Scripts/GameController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private bool isGameRunning;
+
+    public ObstacleGenerator generator;
+    public GameConfiguration config;
+
+    void Start()
+    {
+        isGameRunning = false;
+
+        GameStart();
+    }
+
+    void GameStart()
+    {
+        isGameRunning = true;
+
+        config.speed = 4f;
+        generator.GenerateObstacles();
+
+    }
+
+    public void GameOver()
+    {
+        print("o jogo acabou!");
+
+        isGameRunning = false;
+
+        config.speed = 0f;
+        generator.StopGenerator();
+
+    }
+
+
+}
